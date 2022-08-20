01864896799c896d6cb16d19d11592f6c686ed2d
Adeeba <66249433+Adeeba659@users.noreply.github.com>
2022-08-20 17:17:41 +0500

    Add files via upload

---
+++ onemonthjquery/project-2/index.js
@@ -0,0 +1,26 @@
+$(".question").click(function(){
+
+	$(this).next().fadeToggle("fast");
+	$(this).children().toggleClass("collapse");
+})
+
+// $("#q1").click(function(){
+
+// 	$("#a1").slideToggle("slow");
+// 	$("#arrow1-down, #arrow1-up").toggleClass("collapse");
+
+// })
+
+// $("#q2").click(function(){
+
+// 	$("#a2").slideToggle("slow");
+// 	$("#arrow2-down, #arrow2-up").toggleClass("collapse");
+
+// })
+
+// $("#q3").click(function(){
+
+// 	$("#a3").slideToggle("slow");
+// 	$("#arrow3-down, #arrow3-up").toggleClass("collapse");
+
+// })
